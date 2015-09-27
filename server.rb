40436242b157b527e28d682ee0c4d05599b34979
German Escobar <german.escobarc@gmail.com>
2015-09-27 00:06:55 -0500

    Include threads

--- server.rb
+++ server.rb
@@ -2,15 +2,18 @@ require 'socket'
 
 server = TCPServer.new 2000
 
-loop do
-	client = server.accept
- 
-	request = client.gets.chomp
+def handle_connection(client)
+  request = client.gets.chomp
   puts request
 
-	client.puts "HTTP/1.1 200 OK"
+  client.puts "HTTP/1.1 200 OK"
   client.puts ""
   client.puts "<h1>Hola Mundo</h1>"
 
-	client.close
+  client.close
+end
+
+loop do
+	client = server.accept
+  Thread.new { handle_connection(client) }
 end
